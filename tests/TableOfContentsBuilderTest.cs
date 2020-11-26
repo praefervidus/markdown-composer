a112451799f967e19036f328c116628a4a5a32b3
praefervidus <e.fleming1234@gmail.com>
2020-11-26 00:35:17 -0800

    add test project

---
+++ tests/TableOfContentsBuilderTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace markdown_composer.Tests
+{
+    public class TableOfContentsBuilderTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
